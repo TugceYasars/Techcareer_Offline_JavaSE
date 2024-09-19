95a5b4c20d6e98a8b8f35bc6fc95189ff0c31a1b
Tugce.Yasar <tugceyasar34@outlook.com>
2024-09-19 10:38:14 +0300

    Java Operators, Veriables, psvm

---
+++ src/main/java/org/example/_2_PsvmOperators.java
@@ -0,0 +1,54 @@
+package org.example;
+
+public class _2_PsvmOperators {
+    public static void main(String[] args) {
+//        Atama operatörü
+
+        int number=10;
+        System.out.println(number);
+
+//        Artırma azaltma operatörleri
+//        number++;
+//        number=number+1;
+//        number+=1;
+        System.out.println("Artırma:  "+number);
+
+        number--;
+
+        System.out.println("Azaltma:  "+number);
+
+//      Araştırma
+//        number++;
+//        ++number;
+
+//        Dört işlem
+        int number1=20;
+        int number2=3;
+
+        System.out.println("Toplam: "+(number1+number2));
+        System.out.println("Çıkarma: "+(number1-number2));
+        System.out.println("Çarpma: "+(number1*number2));
+        System.out.println("Bölme: "+(number1/number2));
+        System.out.println("Kalan: "+(number1%number2));
+
+//        Karşılaştırma operatörü
+//        boolean isResult = (number1 == number2);
+//        System.out.println(isResult);
+
+//        > >= < <=
+
+//        Mantıksal Operatörler
+//        VE &&
+//        VEYA||
+//        DEĞİL!
+
+//        Bitsel Operatörler
+//        VE&
+//        VEYA|
+//        DEĞİL!
+
+
+
+
+    }
+}
---
+++ src/main/java/org/example/_3_Veriables.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class _3_Veriables {
+    public static void main(String[] args) {
+//      Java Veriables
+
+        int number=10;
+        System.out.println(number);
+
+        int $number=11;
+        System.out.println($number);
+
+        int _number=12;
+        System.out.println(_number);
+
+/*Değikenleri yazarken(Common Roles) ;
+camelCase
+PascalCase
+Java syntax yapısı olarak C dilini miras almıştır
+ */
+
+
+    }
+}
