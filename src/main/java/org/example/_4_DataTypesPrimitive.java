c2b278376622e5c54ef8c040fd7c01d11ac01fb5
Tugce.Yasar <tugceyasar34@outlook.com>
2024-09-24 12:05:03 +0300

    Primitive and Wrapper Class

--- src/main/java/org/example/_4_DataTypesPrimitive.java
+++ src/main/java/org/example/_4_DataTypesPrimitive.java
@@ -4,15 +4,17 @@ public class _4_DataTypesPrimitive {
     public static void main(String[] args) {
   //Primitive
         //Tam sayılar
-        //
         byte b1=127; //-128<=x<=+127 (8 bit)
         short s1=32767; // -32.768<=x<=+32.767 (16 bit)
         int i1=2147483647; //-2.147.483.648<=x<=+2.147.483.647 (32 bit)
-        long l1=32768; //-9.223.372.036.858<=x<=+-9.223.372.036.857 (364 bit)
+        long l1=32768L; //-9.223.372.036.858<=x<=+-9.223.372.036.857 (364 bit)
 
 
 //Virgüllü
         float f2=14.53f; // (32bit)
+        System.out.println(f2);
+        float f2Data=14.53F;
+        System.out.println(f2Data);
         double d2=14.53; // (64bit)
 
         //boolean
